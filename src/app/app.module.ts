0c0c9c7519a66d7933de72416e3902fa2d6c1da5
LucasRufo <rifelu25@outlook.com>
2020-07-15 19:02:45 -0300

    Finishing the filter view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Navegacao/header/header.component';
+import { MainComponent } from './Paises/main/main.component';
+import { routerConfig } from './app.routes';
+import { FilterComponent } from './Paises/filter/filter.component';
+import { ListComponent } from './Paises/list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MainComponent,
+    FilterComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
     TooltipModule.forRoot(),
+    [RouterModule.forRoot(routerConfig, { useHash: false })]
   ],
-  providers: [],
+  providers: [
+    {provide: APP_BASE_HREF, useValue: '/'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router'
+import { MainComponent } from './Paises/main/main.component';
+
+export const routerConfig: Routes = [
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+  { path: 'main', component: MainComponent }
+];
+
+
